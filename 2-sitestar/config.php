651843f80b8685d1dd978083eb48b64b6e1724c0
liujb <liujiangbei@126.com>
2013-09-17 23:34:24 +0800

    test

---
+++ 2-sitestar/config.php
@@ -0,0 +1,14 @@
+<?php 
+if (!defined('IN_CONTEXT')) die('access violation error!');
+class Config {
+public static $mysql_ext = 'mysql';
+public static $db_host = 'localhost';
+public static $db_user = 'root';
+public static $db_pass = 'liu6631959';
+public static $db_name = 'sitestar';
+public static $port = '3306';
+public static $mysqli_charset = 'utf8';
+public static $tbl_prefix = 'ss_';
+public static $cookie_prefix = 'FeiYd0_';
+public static $enable_db_debug = false;
+}?>
---
+++ 4-test/201306/strFunc-ALLEN-PC.php
@@ -0,0 +1,4 @@
+<?php
+//printf
+printf("Hi,I'm %d years older.",24);
+?>
