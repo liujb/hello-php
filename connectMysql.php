4d9804dc28a1688b7f7a8caf62f461c08f109354
liujb <liujiangbei@126.com>
2013-11-25 18:05:17 +0800

    modify something

--- connectMysql.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$connect =new mysqli("localhost","root","liu!@#631959_","test"); 
-	if(!$connect) {
-		echo "Mysql Connect Error!"; 
-	}else {
-		echo "mysql success"; 
-	}
-	$connect->close();
-?>
