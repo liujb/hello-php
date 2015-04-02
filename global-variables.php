0a07bd0bd636a140238bc5352808f09ed681c753
liujb <liujiangbei@126.com>
2015-04-02 14:18:50 +0800

    add files

---
+++ global-variables.php
@@ -0,0 +1,17 @@
+<?php
+
+$abc = "fucking you";
+
+if (!function_exists('print_global')) {
+    function print_global()
+    {
+        // echo $abc; // 语法错误
+
+        global $abc;
+        echo 'global variables ' . $abc . "\r\n";
+
+    }
+
+}
+
+print_global();
