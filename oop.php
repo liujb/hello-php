d2cbb487e54cd22d720a63466dbd8fb936daa6dc
liujb <liujiangbei@126.com>
2015-10-01 18:03:33 +0800

    modify

--- oop.php
+++ oop.php
@@ -1,22 +1,24 @@
 <?php
-class Base {
-	public $name;
+class Base
+{
+  public $name;
+  
+  // 构造函数
+  public function Base($n) {
+    $this->name = $n;
+  }
+  
+  // function
+  public function sayHello() {
+    echo 'Hi nice to meet you ' . $this->name;
+  }
+}
 
-	//构造函数
-	public function Base($n) {
-		$this->name = $n;
-	}
-
-	//function
-	public function sayHello() {
-		echo 'Hi nice to meet you ' . $this->name;
-	}
-};
-
-class Child extends Base {
-	public function sayHello() {
-		echo 'Hi，this is my son say hello to you ';
-	}
+class Child extends Base
+{
+  public function sayHello() {
+    echo 'Hi，this is my son say hello to you ';
+  }
 }
 
 $p = new Base('Allen');
